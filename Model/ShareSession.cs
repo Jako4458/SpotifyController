3c7c37591c7dc752ac68774c0b53039d3a337eb5
Jakob <jakob.hyldgaard@gmail.com>
2022-04-18 16:05:02 +0200

    added new branch for Sessions

---
+++ Model/ShareSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyController.Model
+{
+    public class ShareSession
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public User User { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsPublic { get; set; }
+        public string? Password { get; set; }
+        public string Url { get; set; }
+        
+    }
+}
